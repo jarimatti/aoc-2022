0e7da0da06834ed453d7fb058ef80349588fcad6
Jarimatti Valkonen <jarimatti@me.com>
2022-12-08 19:47:55 +0000

    Day 8, part 2.

--- lib/aoc2022/day8.ex
+++ lib/aoc2022/day8.ex
@@ -1,11 +1,12 @@
 defmodule AoC2022.Day8 do
   def read_input(path \\ "priv/day8/test.txt") do
-    {max_row, max_col, map} = path
-    |> File.stream!()
-    |> Stream.map(&String.trim/1)
-    |> parse_map()
+    {max_row, max_col, map} =
+      path
+      |> File.stream!()
+      |> Stream.map(&String.trim/1)
+      |> parse_map()
 
-    {{0..(max_row-1), 0..(max_col-1)}, map}
+    {{0..(max_row - 1), 0..(max_col - 1)}, map}
   end
 
   defp parse_map(lines) do
@@ -33,35 +34,42 @@ defmodule AoC2022.Day8 do
 
   defp visible_trees({{row_range, col_range}, map}) do
     # Go through rows and grab visible trees.
-    row_set = Enum.reduce(row_range, MapSet.new(), fn row, set ->
-      {_, new_set} = Enum.reduce(col_range, {-1, set}, fn col, {prev, set} ->
-        visible_step(row, col, prev, map, set)
+    row_set =
+      Enum.reduce(row_range, MapSet.new(), fn row, set ->
+        {_, new_set} =
+          Enum.reduce(col_range, {-1, set}, fn col, {prev, set} ->
+            visible_step(row, col, prev, map, set)
+          end)
+
+        new_set
       end)
 
-      new_set
-    end)
+    row_set =
+      Enum.reduce(row_range, row_set, fn row, set ->
+        {_, new_set} =
+          Enum.reduce(Enum.reverse(col_range), {-1, set}, fn col, {prev, set} ->
+            visible_step(row, col, prev, map, set)
+          end)
 
-    row_set = Enum.reduce(row_range, row_set, fn row, set ->
-      {_, new_set} = Enum.reduce(Enum.reverse(col_range), {-1, set}, fn col, {prev, set} ->
-        visible_step(row, col, prev, map, set)
+        new_set
       end)
-
-      new_set
-    end)
 
     # Go through columns and grab visible trees.
-    col_set = Enum.reduce(col_range, row_set, fn col, set ->
-      {_, new_set} = Enum.reduce(row_range, {-1, set}, fn row, {prev, set} ->
-        visible_step(row, col, prev, map, set)
-      end)
+    col_set =
+      Enum.reduce(col_range, row_set, fn col, set ->
+        {_, new_set} =
+          Enum.reduce(row_range, {-1, set}, fn row, {prev, set} ->
+            visible_step(row, col, prev, map, set)
+          end)
 
-      new_set
-    end)
+        new_set
+      end)
 
     Enum.reduce(col_range, col_set, fn col, set ->
-      {_, new_set} = Enum.reduce(Enum.reverse(row_range), {-1, set}, fn row, {prev, set} ->
-        visible_step(row, col, prev, map, set)
-      end)
+      {_, new_set} =
+        Enum.reduce(Enum.reverse(row_range), {-1, set}, fn row, {prev, set} ->
+          visible_step(row, col, prev, map, set)
+        end)
 
       new_set
     end)
@@ -81,7 +89,36 @@ defmodule AoC2022.Day8 do
   end
 
   def part2(path \\ "priv/day8/test.txt") do
-    path
-    |> read_input()
+    {{row_range, col_range}, map} = read_input(path)
+
+    all_points = for row <- row_range, col <- col_range, do: {row, col}
+
+    all_points
+    |> Enum.map(fn p -> scenic_score(p, {row_range, col_range}, map) end)
+    |> Enum.max()
+  end
+
+  def scenic_score({row, col}, {row_range, col_range}, map) do
+    height = Map.get(map, {row, col})
+
+    points_north = Enum.reverse(for r <- row_range, r < row, do: {r, col})
+    points_south = for r <- row_range, r > row, do: {r, col}
+    points_west = Enum.reverse(for c <- col_range, c < col, do: {row, c})
+    points_east = for c <- col_range, c > col, do: {row, c}
+
+    [points_north, points_south, points_west, points_east]
+    |> Enum.map(fn r -> visible_range(height, r, map) end)
+    |> Enum.product()
+  end
+
+  def visible_range(_height, [], _map), do: 0
+
+  def visible_range(height, points, map) do
+    points
+    |> Enum.map(fn p -> Map.get(map, p) end)
+    |> Enum.reduce_while(0, fn
+      t, count when t >= height -> {:halt, count + 1}
+      _t, count -> {:cont, count + 1}
+    end)
   end
 end
--- test/aoc2022/day8_test.exs
+++ test/aoc2022/day8_test.exs
@@ -11,5 +11,11 @@ defmodule Aoc2022.Day8Test do
     assert(Day8.part1("priv/day8/input.txt") == 1785)
   end
 
+  test "part 2 test input solution is correct" do
+    assert(Day8.part2("priv/day8/test.txt") == 8)
+  end
 
+  test "part 2 solution is correct" do
+    assert(Day8.part2("priv/day8/input.txt") == 345_168)
+  end
 end
