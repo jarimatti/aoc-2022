3d2f0387790b1e69383fa388111b6a76b4affa45
Jarimatti Valkonen <jarimatti@me.com>
2022-12-11 15:12:19 +0000

    Day 10, part 2 solved.

--- lib/aoc2022/day10.ex
+++ lib/aoc2022/day10.ex
@@ -58,8 +58,69 @@ defmodule AoC2022.Day10 do
     |> Enum.sum()
   end
 
+  defmodule Crt do
+    def new() do
+      []
+    end
+
+    def draw_pixel(crt, :lit) do
+      [:lit | crt]
+    end
+
+    def draw_pixel(crt, :dark) do
+      [:dark | crt]
+    end
+
+    def render(crt) do
+      crt
+      |> Enum.reverse()
+      |> Enum.map(&pixel_to_string/1)
+      |> Enum.chunk_every(40)
+      |> Enum.join("\n")
+    end
+
+    defp pixel_to_string(:lit), do: "#"
+    defp pixel_to_string(:dark), do: "."
+  end
+
   def part2(path \\ "priv/day10/test.txt") do
-    path
-    |> read_input()
+    states =
+      path
+      |> read_input()
+      |> execute(new_machine())
+      |> Enum.to_list()
+
+    max_clock = List.last(states)
+
+    timestamps = 0..(max_clock.clock - 1) |> IO.inspect()
+
+    {crt, _} =
+      timestamps
+      |> Enum.reduce({Crt.new(), states}, fn ts, {crt, states} ->
+        current_state = hd(states)
+
+        new_crt = Crt.draw_pixel(crt, pixel(current_state.x, ts))
+
+        new_states =
+          if current_state.clock <= ts do
+            tl(states)
+          else
+            states
+          end
+
+        {new_crt, new_states}
+      end)
+
+    Crt.render(crt)
+  end
+
+  defp pixel(x, ts) do
+    scan_pos = rem(ts, 40)
+
+    if abs(x - scan_pos) <= 1 do
+      :lit
+    else
+      :dark
+    end
   end
 end
