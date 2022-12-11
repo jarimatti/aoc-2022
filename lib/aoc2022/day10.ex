3b9a086fd11bda89cd5f9e29a3a0e199c554a118
Jarimatti Valkonen <jarimatti@me.com>
2022-12-11 14:49:28 +0000

    Day 10, part 1.

---
+++ lib/aoc2022/day10.ex
@@ -0,0 +1,62 @@
+defmodule AoC2022.Day10 do
+
+  def read_input(path \\ "priv/day10/test.txt") do
+    path
+    |> File.stream!()
+    |> Stream.map(&String.trim/1)
+    |> Stream.map(&parse_op/1)
+  end
+
+  defp parse_op("noop"), do: :noop
+
+  defp parse_op("addx " <> rest) do
+    {:addx, String.to_integer(rest)}
+  end
+
+  def new_machine() do
+    %{
+      x: 1,
+      clock: 1
+    }
+  end
+
+  def apply_instruction(machine, :noop) do
+    %{machine | clock: machine.clock + 1}
+  end
+
+  def apply_instruction(machine, {:addx, amount}) do
+    %{machine | clock: machine.clock + 2, x: machine.x + amount}
+  end
+
+  def execute(instructions, machine) do
+    Stream.scan(instructions, machine, fn op, m -> apply_instruction(m, op) end)
+  end
+
+  def part1(path \\ "priv/day10/test.txt") do
+    states = path
+    |> read_input()
+    |> execute(new_machine())
+    |> Enum.to_list()
+
+    max_clock = List.last(states)
+
+    timestamps = 20..max_clock.clock//40
+
+    {values, _} = timestamps
+    |> Enum.reduce({[], states}, fn ts, {xs, sts} ->
+      [s | _] = rest = Enum.drop_while(sts, fn %{clock: c} -> c < ts-1 end)
+
+      {[Map.put(s, :at, ts) | xs], rest}
+    end)
+
+    values
+    |> Enum.map(fn s -> s.at * s.x end)
+    |> Enum.sum()
+  end
+
+  def part2(path \\ "priv/day10/test.txt") do
+    path
+    |> read_input()
+  end
+
+end
---
+++ test/aoc2022/day10_test.exs
@@ -0,0 +1,14 @@
+defmodule Aoc2022.Day10Test do
+  use ExUnit.Case
+
+  alias AoC2022.Day10
+
+  test "part 1 test input solution is correct" do
+    assert(Day10.part1("priv/day10/test.txt") == 13140)
+  end
+
+  test "part 1 input solution is correct" do
+    assert(Day10.part1("priv/day10/input.txt") == 14520)
+  end
+
+end
