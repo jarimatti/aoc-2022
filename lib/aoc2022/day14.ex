e40eb76fa0f33423a8e6b6928410d6f97564d4a7
Jarimatti Valkonen <jarimatti@me.com>
2022-12-14 08:44:57 +0000

    Day 14: render cave to string for display

--- lib/aoc2022/day14.ex
+++ lib/aoc2022/day14.ex
@@ -1,10 +1,13 @@
 defmodule AoC2022.Day14 do
   def read_input(path \\ "priv/day14/test.txt") do
-    path
-    |> File.stream!()
-    |> Stream.map(&String.trim/1)
-    |> Stream.map(&parse_path/1)
-    |> Enum.reduce(%{}, &add_path_to_cave/2)
+    cave =
+      path
+      |> File.stream!()
+      |> Stream.map(&String.trim/1)
+      |> Stream.map(&parse_path/1)
+      |> Enum.reduce(%{}, &add_path_to_cave/2)
+
+    Map.put(cave, {500, 0}, :start)
   end
 
   defp add_path_to_cave(path, cave) do
@@ -41,6 +44,40 @@ defmodule AoC2022.Day14 do
       |> Enum.map(&String.to_integer/1)
 
     {x, y}
+  end
+
+  def cave_to_string(cave) do
+    points = Map.keys(cave)
+
+    {min_x, max_x} =
+      points
+      |> Enum.map(fn {x, _} -> x end)
+      |> Enum.min_max()
+
+    {min_y, max_y} =
+      points
+      |> Enum.map(fn {_, y} -> y end)
+      |> Enum.min_max()
+
+    top_left = {min_x, min_y}
+    bottom_right = {max_x, max_y}
+
+    {top_left, bottom_right}
+
+    Enum.map(min_y..max_y, fn y ->
+      Enum.map(
+        min_x..max_x,
+        fn x ->
+          case Map.get(cave, {x, y}, :empty) do
+            :empty -> "."
+            :rock -> "#"
+            :sand -> "o"
+            :start -> "+"
+          end
+        end
+      )
+    end)
+    |> Enum.join("\n")
   end
 
   def part1(path \\ "priv/day14/test.txt") do
